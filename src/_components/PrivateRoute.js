f55fed5d26b4bfebf87bf9641ec7b44937c9cb85
del <kingston.coker@statista.com>
2021-05-03 05:05:56 +0200

    fixed private route

--- src/_components/PrivateRoute.js
+++ src/_components/PrivateRoute.js
@@ -5,13 +5,10 @@ import { accountService } from '../_services';
 
 function PrivateRoute({component: Component, /**roles,**/ ...rest}) {
 
-		// const b = getUserSession;
-		// console.log(b)
 		return (
 			<Route {...rest} render={props => {
 
 					const user = accountService.getUserSession();
-					console.log(user)
 					if (!user) {
 							// not logged in so redirect to login page with the return url
 							return <Redirect to={{pathname: '/login', state: {from: props.location}}}/>
