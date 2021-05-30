54117fcd4712083fbd70094ffab6e3f5cc0cd52d
del <kingston.coker@statista.com>
2021-05-30 17:45:14 +0200

    fe baseUrl axiosConfig

--- src/_helpers/axiosConfig.js
+++ src/_helpers/axiosConfig.js
@@ -5,7 +5,7 @@ const axiosConfig = axios.create({
   //     process.env.REACT_APP_DEV_BACKEND_API_URL :
   //     process.env.REACT_APP_PROD_BACKEND_API_URL,
   // baseURL: 'http://localhost:8000',//http(s)://127.0.0.1
-  baseURL: 'http://192.168.2.108:8000',
+  baseURL: 'https://dwm-social-media-backend.herokuapp.com/',
 });
 
 export default axiosConfig;
