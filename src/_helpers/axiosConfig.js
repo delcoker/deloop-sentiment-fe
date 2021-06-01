37952a1c1d5605057dfb07427b766f757c027354
del <kingston.coker@statista.com>
2021-06-01 22:47:03 +0200

    getting rid of yarn, axios beurl

--- src/_helpers/axiosConfig.js
+++ src/_helpers/axiosConfig.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 // Next we make an 'instance' of it
 const axiosConfig = axios.create({
-  // baseURL: process.env.NODE_ENV === "development" ?
-  //     process.env.REACT_APP_DEV_BACKEND_API_URL :
-  //     process.env.REACT_APP_PROD_BACKEND_API_URL,
-  // baseURL: 'http://localhost:8000',//http(s)://127.0.0.1
-  baseURL: 'http://192.168.2.108:2222',
+    // baseURL: process.env.NODE_ENV === "development" ?
+    //     process.env.REACT_APP_DEV_BACKEND_API_URL :
+    //     process.env.REACT_APP_PROD_BACKEND_API_URL,
+    // baseURL: 'http://localhost:8000',//http(s)://127.0.0.1,
+    // baseURL: 'http://192.168.2.108:2222',
+    baseURL: 'https://dwm-social-media-backend.herokuapp.com/',
 });
 
 export default axiosConfig;
