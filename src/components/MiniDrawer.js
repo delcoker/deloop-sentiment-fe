58c417c04af3e7f1f038092c70c08c16fc701a8b
del <kingston.coker@statista.com>
2021-06-03 19:45:29 +0200

    i

--- src/components/MiniDrawer.js
+++ src/components/MiniDrawer.js
@@ -22,7 +22,7 @@ import {Link, useLocation} from "react-router-dom";
 
 import routes from "../routers/routes";
 import Header from "./headers/Header";
-// import useStyles from "../_helpers/use_styles/styles";
+import useStyles from "../_helpers/use_styles/styles";
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -34,69 +34,69 @@ import {accountService} from "../_services";
 import SubHeaderComponent from "./headers/SubHeaderComponent";
 
 
-const drawerWidth = 220;
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-    },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginRight: 36,
-    },
-    hide: {
-        display: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-    },
-    drawerOpen: {
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    drawerClose: {
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        overflowX: 'hidden',
-        width: theme.spacing(7) + 1,
-        [theme.breakpoints.up('sm')]: {
-            width: theme.spacing(9) + 1,
-        },
-    },
-    toolbar: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-    },
-}));
+// const drawerWidth = 220;
+//
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         display: 'flex',
+//     },
+//     appBar: {
+//         zIndex: theme.zIndex.drawer + 1,
+//         transition: theme.transitions.create(['width', 'margin'], {
+//             easing: theme.transitions.easing.sharp,
+//             duration: theme.transitions.duration.leavingScreen,
+//         }),
+//     },
+//     appBarShift: {
+//         marginLeft: drawerWidth,
+//         width: `calc(100% - ${drawerWidth}px)`,
+//         transition: theme.transitions.create(['width', 'margin'], {
+//             easing: theme.transitions.easing.sharp,
+//             duration: theme.transitions.duration.enteringScreen,
+//         }),
+//     },
+//     menuButton: {
+//         marginRight: 36,
+//     },
+//     hide: {
+//         display: 'none',
+//     },
+//     drawer: {
+//         width: drawerWidth,
+//         flexShrink: 0,
+//         whiteSpace: 'nowrap',
+//     },
+//     drawerOpen: {
+//         width: drawerWidth,
+//         transition: theme.transitions.create('width', {
+//             easing: theme.transitions.easing.sharp,
+//             duration: theme.transitions.duration.enteringScreen,
+//         }),
+//     },
+//     drawerClose: {
+//         transition: theme.transitions.create('width', {
+//             easing: theme.transitions.easing.sharp,
+//             duration: theme.transitions.duration.leavingScreen,
+//         }),
+//         overflowX: 'hidden',
+//         width: theme.spacing(7) + 1,
+//         [theme.breakpoints.up('sm')]: {
+//             width: theme.spacing(9) + 1,
+//         },
+//     },
+//     toolbar: {
+//         display: 'flex',
+//         alignItems: 'center',
+//         justifyContent: 'flex-end',
+//         padding: theme.spacing(0, 1),
+//         // necessary for content to be below app bar
+//         ...theme.mixins.toolbar,
+//     },
+//     content: {
+//         flexGrow: 1,
+//         padding: theme.spacing(3),
+//     },
+// }));
 
 export default function MiniDrawer({children, pageTitle, showSubheader}) {
     const classes = useStyles();
@@ -132,62 +132,6 @@ export default function MiniDrawer({children, pageTitle, showSubheader}) {
                     pageTitle={pageTitle}
                     showSubheader={showSubheader}
                     classes={classes} />
-
-            {/*<AppBar*/}
-            {/*    position="fixed"*/}
-            {/*    className={clsx(classes.appBar, {*/}
-            {/*        [classes.appBarShift]: open,*/}
-            {/*    })}*/}
-            {/*>*/}
-            {/*    <Toolbar>*/}
-            {/*        <IconButton*/}
-            {/*            color="inherit"*/}
-            {/*            aria-label="open drawer"*/}
-            {/*            onClick={handleDrawerOpen}*/}
-            {/*            edge="start"*/}
-            {/*            className={clsx(classes.menuButton, {*/}
-            {/*                [classes.hide]: open,*/}
-            {/*            })}*/}
-            {/*        >*/}
-            {/*            /!*{accountService.getUserSession()*!/*/}
-            {/*            /!*&& *!/*/}
-            {/*            <MenuIcon />*/}
-            {/*            */}
-            {/*        </IconButton>*/}
-            {/*        <Typography variant="h6" color="inherit" noWrap className={classes.title}>*/}
-            {/*            DWM Sentimento {pageTitle}*/}
-            {/*        </Typography>*/}
-            {/*        <div className={classes.spaceOut} />*/}
-            {/*        {accountService.getUserSession() && <Button*/}
-            {/*            component="a"*/}
-            {/*            href="https://github.com/dwm-codebase"*/}
-            {/*            target="_blank"*/}
-            {/*            endIcon={<DescriptionIcon />}*/}
-            {/*            color="inherit"*/}
-            {/*        >*/}
-            {/*            Source Code : FE*/}
-            {/*        </Button>}*/}
-            {/*        <Button*/}
-            {/*            component="a"*/}
-            {/*            href="https://github.com/dwm-codebase/fe_final"*/}
-            {/*            endIcon={<GitHubIcon />}*/}
-            {/*            color="inherit"*/}
-            {/*            target="_blank"*/}
-            {/*        >*/}
-            {/*            Project*/}
-            {/*        </Button>*/}
-            {/*        {accountService.getUserSession() && <Button*/}
-            {/*            endIcon={<TimeToLeave />}*/}
-            {/*            color="inherit"*/}
-            {/*            onClick={accountService.logout}*/}
-            {/*        >*/}
-            {/*            Logout*/}
-            {/*        </Button>}*/}
-            {/*    </Toolbar>*/}
-
-            {/*    {showSubheader && <SubHeaderComponent />}*/}
-
-            {/*</AppBar>*/}
 
             <Drawer variant="permanent"
                     className={clsx(classes.drawer, {
