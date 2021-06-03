280945dd75cca12b4e7146f2c058d82c9d3c9887
del <kingston.coker@statista.com>
2021-06-03 21:37:39 +0200

    header

--- src/components/MiniDrawer.js
+++ src/components/MiniDrawer.js
@@ -2,27 +2,13 @@ import React from 'react';
 import clsx from 'clsx';
 import {makeStyles, useTheme} from '@material-ui/core/styles';
 import {matchPath} from "react-router";
-import {
-    Button,
-    CssBaseline,
-    Divider,
-    Drawer,
-    IconButton,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText
-} from '@material-ui/core';
-import {
-    ChevronLeft as ChevronLeftIcon,
-    ChevronRight as ChevronRightIcon,
-    Description as DescriptionIcon, GitHub as GitHubIcon, TimeToLeave,
-} from '@material-ui/icons';
+import {CssBaseline, Divider, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText} from '@material-ui/core';
+import {ChevronLeft as ChevronLeftIcon, ChevronRight as ChevronRightIcon,} from '@material-ui/icons';
 import {Link, useLocation} from "react-router-dom";
 
 import routes from "../routers/routes";
 import Header from "./headers/Header";
-import useStyles from "../_helpers/use_styles/styles";
+// import useStyles from "../_helpers/use_styles/styles";
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -30,73 +16,75 @@ import Typography from '@material-ui/core/Typography';
 import MenuIcon from '@material-ui/icons/Menu';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
-import {accountService} from "../_services";
-import SubHeaderComponent from "./headers/SubHeaderComponent";
 
 
-// const drawerWidth = 220;
-//
-// const useStyles = makeStyles((theme) => ({
-//     root: {
-//         display: 'flex',
-//     },
-//     appBar: {
-//         zIndex: theme.zIndex.drawer + 1,
-//         transition: theme.transitions.create(['width', 'margin'], {
-//             easing: theme.transitions.easing.sharp,
-//             duration: theme.transitions.duration.leavingScreen,
-//         }),
-//     },
-//     appBarShift: {
-//         marginLeft: drawerWidth,
-//         width: `calc(100% - ${drawerWidth}px)`,
-//         transition: theme.transitions.create(['width', 'margin'], {
-//             easing: theme.transitions.easing.sharp,
-//             duration: theme.transitions.duration.enteringScreen,
-//         }),
-//     },
-//     menuButton: {
-//         marginRight: 36,
-//     },
-//     hide: {
-//         display: 'none',
-//     },
-//     drawer: {
-//         width: drawerWidth,
-//         flexShrink: 0,
-//         whiteSpace: 'nowrap',
-//     },
-//     drawerOpen: {
-//         width: drawerWidth,
-//         transition: theme.transitions.create('width', {
-//             easing: theme.transitions.easing.sharp,
-//             duration: theme.transitions.duration.enteringScreen,
-//         }),
-//     },
-//     drawerClose: {
-//         transition: theme.transitions.create('width', {
-//             easing: theme.transitions.easing.sharp,
-//             duration: theme.transitions.duration.leavingScreen,
-//         }),
-//         overflowX: 'hidden',
-//         width: theme.spacing(7) + 1,
-//         [theme.breakpoints.up('sm')]: {
-//             width: theme.spacing(9) + 1,
-//         },
-//     },
-//     toolbar: {
-//         display: 'flex',
-//         alignItems: 'center',
-//         justifyContent: 'flex-end',
-//         padding: theme.spacing(0, 1),
-//         // necessary for content to be below app bar
-//         ...theme.mixins.toolbar,
-//     },
-//     content: {
-//         flexGrow: 1,
-//         padding: theme.spacing(3),
-//     },
-// }));
+const drawerWidth = 220;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerClose: {
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: theme.spacing(7) + 1,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9) + 1,
+        },
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+    spaceOut: {
+        flex: '1 1 auto',
+        marginLeft: drawerWidth
+    },
+}));
 
 export default function MiniDrawer({children, pageTitle, showSubheader}) {
     const classes = useStyles();
@@ -132,6 +120,30 @@ export default function MiniDrawer({children, pageTitle, showSubheader}) {
                     pageTitle={pageTitle}
                     showSubheader={showSubheader}
                     classes={classes} />
+
+            {/*<AppBar*/}
+            {/*    position="fixed"*/}
+            {/*    className={clsx(classes.appBar, {*/}
+            {/*        [classes.appBarShift]: open,*/}
+            {/*    })}*/}
+            {/*>*/}
+            {/*    <Toolbar>*/}
+            {/*        <IconButton*/}
+            {/*            color="inherit"*/}
+            {/*            aria-label="open drawer"*/}
+            {/*            onClick={handleDrawerOpen}*/}
+            {/*            edge="start"*/}
+            {/*            className={clsx(classes.menuButton, {*/}
+            {/*                [classes.hide]: open,*/}
+            {/*            })}*/}
+            {/*        >*/}
+            {/*            <MenuIcon />*/}
+            {/*        </IconButton>*/}
+            {/*        <Typography variant="h6" noWrap>*/}
+            {/*            Mini variant drawer*/}
+            {/*        </Typography>*/}
+            {/*    </Toolbar>*/}
+            {/*</AppBar>*/}
 
             <Drawer variant="permanent"
                     className={clsx(classes.drawer, {
